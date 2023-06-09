7eef2c1457d52eda5c36b146756c95bbbb7b8f2a
mercen4i <132190517+mercen4i@users.noreply.github.com>
2023-06-09 18:06:15 +0800

    Add a little UI and style
    - Create a function that clears the canvas
    - Create a function that create squares
    - Create a function that removes squares
    - Add an event listener that activates when mouse is hovered over the grid
    - Add a clear button that clears the canvas
    - Add a grid size controller that changes the sizes of the squares in the container

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 const container = document.querySelector('.container');
-const numberOfSquares = 50;
-const percentage = 100/numberOfSquares;
+const sizeController = document.querySelector('#size-controller');
+let numberOfSquares = sizeController.value;
+let percentage = 100/numberOfSquares;
+sizeController.addEventListener('change', function() {
+    sizeController.setAttribute('value', sizeController.value);
+    numberOfSquares = sizeController.value;
+    percentage = 100/numberOfSquares;
+    squares = document.querySelectorAll('.square');
+    clearCanvas();
+    removeSquares();
+    createSquares();
+    squares = document.querySelectorAll('.square');
+});
 
-for (let i = 0; i < numberOfSquares*numberOfSquares; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.margin = '0';
-    square.style.width = `${percentage}%`;
-    square.style.height = `${percentage}%`;
-    square.addEventListener('mouseenter', () => {square.style.backgroundColor = 'black';});
-    container.appendChild(square); 
+function createSquares() {
+    for (let i = 0; i < numberOfSquares*numberOfSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.margin = '0';
+        square.style.width = `${percentage}%`;
+        square.style.height = `${percentage}%`;
+        square.addEventListener('mouseenter', () => {square.style.backgroundColor = '#808080';});
+        container.appendChild(square); 
+    }
 }
 
 function clearCanvas() {
     squares.forEach((square) => {
         square.style.backgroundColor = 'white';
+    });
+}
+
+function removeSquares() {
+    squares.forEach((square) => {
+        container.removeChild(square);
     })
 }
 
-const squares = document.querySelectorAll('.square');
-const clearButton = document.createElement('button');
-clearButton.textContent = "Clear";
-container.appendChild(clearButton);
+createSquares();
+let squares = document.querySelectorAll('.square');
+const clearButton = document.querySelector('.clear-button');
 clearButton.addEventListener('click', clearCanvas);
